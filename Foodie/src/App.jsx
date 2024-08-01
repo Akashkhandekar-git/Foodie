a0f1250c0dc8cfeea995054a783941796105e384
Akash Khandekar <akhandekar@osius.com>
2024-08-01 16:30:52 +0530

    Foodie Project initiated

---
+++ Foodie/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "./Component/Navbar/Navbar";
+import "./index.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Cart from "./Pages/Cart/Cart";
+import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
+import Footer from "./Component/Footer/Footer";
+
+const App = () => {
+  return (
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/placeorder" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ Foodie/src/Component/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Order your favorite food here..!</h2>
+            <p>Choose from a diverse menufeaturing a delectable array of dishes creafted with the finest ingredients</p>
+            <button>View menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
