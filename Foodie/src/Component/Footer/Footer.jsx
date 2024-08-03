0a245cdada658db0315b2d475435f3284aff1d7e
Akash Khandekar <akhandekar@osius.com>
2024-08-04 01:02:37 +0530

    Added Footer.

--- Foodie/src/Component/Footer/Footer.jsx
+++ Foodie/src/Component/Footer/Footer.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React from "react";
 import "./Footer.css";
+import { assets } from "../../assets/assets";
 
 const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
-  )
-}
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            eligendi sequi saepe, nam, optio ad accusantium assumenda aperiam
+            dignissimos autem consequatur, quidem inventore. Dolores voluptatem
+            reprehenderit tenetur deleniti asperiores quod!
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>Company</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>Get in touch</h2>
+          <ul>
+            <li>+91 123-245-2323232</li>
+            <li>contact_tomato@mail.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright © 2024 Tomato. All rights reserved.
+      </p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
