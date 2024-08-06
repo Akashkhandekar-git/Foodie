a76e6fd00cde500b9eb65649f2970b116c92ecbf
Akash Khandekar <akhandekar@osius.com>
2024-08-07 00:08:36 +0530

    Login/SignUp Pop-Up developed and added

--- Foodie/src/App.jsx
+++ Foodie/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Component/Navbar/Navbar";
 import "./index.css";
 import { Routes, Route } from "react-router-dom";
@@ -6,12 +6,17 @@ import Home from "./Pages/Home/Home";
 import Cart from "./Pages/Cart/Cart";
 import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
 import Footer from "./Component/Footer/Footer";
+import LoginPopUp from "./Component/LoginPopUp/LoginPopUp";
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
   return (
     <>
+    {
+      showLogin?<LoginPopUp setShowLogin = {setShowLogin}/>:<></>
+    }
       <div className="app">
-        <Navbar />
+        <Navbar setShowLogin = {setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
